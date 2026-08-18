6d8e1bc7052f51c2010338c8bb2e12b6b018167b
matthew bootdev <peregoywork@gmail.com>
2026-08-17 19:42:21 -0700

    point to Canvas

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+import Canvas from './components/Canvas.vue'
 </script>
 
 <template>
-  <HelloWorld />
+  <Canvas />
 </template>
