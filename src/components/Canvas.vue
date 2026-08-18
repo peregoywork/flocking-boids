ba360eeb93ee42acdedaafe1933f45e719bf72a1
matthew bootdev <peregoywork@gmail.com>
2026-08-17 19:41:35 -0700

    start Canvas file to hold all boids

---
+++ src/components/Canvas.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+    <div class="canvas-wrapper">
+	<canvas></canvas>
+    </div>
+</template>
+
+<style scoped lang="css">
+.canvas-wrapper {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin: 0;
+}
+
+canvas {
+    border: 1px solid blue;
+    background-color: #; 
+    box-shadow: 0 4px 10px rgba(0,0,0,0.3);
+}
+</style>
+
