ed96f585b06d94ea50fbed6a9dcc160355bfc221
matthew bootdev <peregoywork@gmail.com>
2026-08-26 22:28:11 -0700

    update main App

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,43 @@
 <script setup lang="ts">
-import BoidVisual from './components/BoidVisual.vue'
+import { computed, onMounted, ref } from 'vue';
+import { Boid } from '@/types/Boid.ts';
+
+const canvas = ref(null); 
+const ctx = ref(null); 
+
+onMounted(() => {
+  canvas.value = document.getElementById("boid-canvas");
+  ctx.value = canvas.value.getContext("2d");
+
+  console.log(canvas.value);
+  console.log(ctx.value);
+});
+
+const boids = computed(() => {
+  let bs = [];
+
+  for (let i = 0; i < 1; i++) {
+    bs.push(new Boid({
+      position: { x: 50, y: 50 },
+      velocity: { x: 1, y: 1 },
+      acceleration: { x: 1, y: 1 },
+      maxSpeed: 10,
+      rotation: 0,
+      lineLength: 2,
+    }));
+  }
+
+  return bs;
+});
+
+boids.value[0].draw(ctx.value);
 
 </script>
 
 <template>
-  <BoidVisual />
+  <canvas id="boid-canvas" />
 </template>
+
+<style scoped lang="css">
+</style>
+
