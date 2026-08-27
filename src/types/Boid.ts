bbb13232df12cfbf789aef731b61aec9da6e1742
matthew bootdev <peregoywork@gmail.com>
2026-08-26 22:27:27 -0700

    update Boid type

--- src/types/Boid.ts
+++ src/types/Boid.ts
@@ -1,23 +1,45 @@
+
+
 export class Boid {
     position: { x: number, y: number }
     velocity: { x: number, y: number }
     acceleration: { x: number, y: number }
+    maxSpeed: number
+    rotation: number
+    lineLength: number
 
-    constructor(posX: number, posY: number) {
-	this.position = { x: posX, y: posY };
-	this.velocity = { x: 0, y: 0 };
-	this.acceleration = { x: 0, y: 0 };
+    constructor(data: {
+        position: { x: number, y: number },
+        velocity: { x: number, y: number },
+        acceleration: { x: number, y: number },
+        maxSpeed: number,
+        rotation: number,
+        lineLength: number,
+    }) {
+        this.position = data.position;
+        this.velocity = data.velocity;
+        this.acceleration = data.acceleration;
+        this.maxSpeed = data.maxSpeed;
+        this.rotation = data.rotation;
+        this.lineLength = data.lineLength;
+    }
+
+    draw(ctx: CanvasRenderingContext2D): void {
+        console.log(ctx);
+
+        // ctx.strokeRect(this.position.x, this.position.y, this.lineLength, this.lineLength);
+        // ctx.fillRect(130, 190, 40, 60);
     }
 
     update(): void { 
-	this.velocity = {
-	    x: this.velocity.x + this.acceleration.x,
-	    y: this.velocity.y + this.acceleration.y,
-	};
-	this.position = {
-	    x: this.position.x + this.velocity.x,
-	    y: this.position.y + this.velocity.y,
-	};
+        this.velocity = {
+            x: Math.max(this.velocity.x + this.acceleration.x, this.maxSpeed),
+            y: Math.max(this.velocity.y + this.acceleration.y, this.maxSpeed),
+        };
+        this.position = {
+            x: this.position.x + this.velocity.x,
+            y: this.position.y + this.velocity.y,
+        };
     }
 
     // separate(boids: Boid[]): void {}
